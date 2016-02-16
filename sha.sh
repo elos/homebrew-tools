90a9042a4fbfd240b4abe32be765fa211750d114
Nick Landolfi <nclandolfi@gmail.com>
2016-02-15 19:11:12 -0800

    add comments to .sh

--- sha.sh
+++ sha.sh
@@ -2,4 +2,5 @@
 
 set -e
 
+# FIRST ARGUMENT IS THE FILE TO SHA
 shasum -a 256 $1
--- tar.sh
+++ tar.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+# FIRST ARGUMENT IS FILE/BINARY TO GZIP, SECOND IS VERSION
 tar -czf $1-$2.tar.gz $1
 echo $1-$2.tar.gz
