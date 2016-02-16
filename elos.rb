f53c08595ae5f0445ee1f9789782ef8a0e88805b
Nick Landolfi <nclandolfi@gmail.com>
2016-02-15 19:04:57 -0800

    fix SHA

--- elos.rb
+++ elos.rb
@@ -2,7 +2,7 @@ class Elos < Formula
   desc "Elos Command Line Interface"
   homepage "https://github.com/elos/elos"
   url "https://github.com/elos/elos/releases/download/v0.3.1/elos-0.3.1.tar.gz"
-  sha256 "ade32dcfb2c7816faebb2932fe6b54dacf1d055ce4bdd962725a099dacd11185"
+  sha256 "4103c689822a40d899489816c7fbbe0b6aad0c98f57c0f6da2b51bbcf3e1f1a0"
 
   bottle :unneeded
 
